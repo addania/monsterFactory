4bb693ad45f323f864c7d41932e9c83e97db9a81
addania <mirka.schw@gmail.com>
2019-11-29 19:34:20 +0100

    Updates

---
+++ precache-manifest.da9d8649ed0b15e5aea58285e866c95c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83b630a53dba2e2b46f8cf8027d415c7",
+    "url": "/monsterFactory/index.html"
+  },
+  {
+    "revision": "084adafa6621d22db526",
+    "url": "/monsterFactory/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "93ff02e7bdbf460c2dc8",
+    "url": "/monsterFactory/static/js/2.dd52ce2e.chunk.js"
+  },
+  {
+    "revision": "084adafa6621d22db526",
+    "url": "/monsterFactory/static/js/main.50f19426.chunk.js"
+  },
+  {
+    "revision": "1b6108be6e12fe556278",
+    "url": "/monsterFactory/static/js/runtime-main.6987b82d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/monsterFactory/static/media/logo.25bf045c.svg"
+  }
+]);
